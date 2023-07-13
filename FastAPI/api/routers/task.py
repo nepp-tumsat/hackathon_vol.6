d286b5d8789f8f51c76c4c5e9b900ed890935a7c
{Ryu} <{r216af@icloud.com}>
2023-07-14 01:19:46 +0900

    APIの雛形の作成

--- FastAPI/api/routers/task.py
+++ FastAPI/api/routers/task.py
@@ -19,6 +19,12 @@ async def create_task(
 ):
     return await task_crud.create_task(db, task_body)
 
+@router.post("/add_info" )  #名前、職業、趣味、趣味にかけるお金を追加(自由に変更可)
+async def add_info(
+
+):
+    pass
+
 @router.put("/tasks/{task_id}",  response_model=task_schema.TaskCreateResponse)
 async def update_task(
     task_id: int, task_body: task_schema.TaskCreate, db: AsyncSession = Depends(get_db)
@@ -28,6 +34,18 @@ async def update_task(
         raise HTTPException(stasus_code=404, detail="Task not found")
     
     return await task_crud.update_task(db, task_body, original=task)
+
+@router.put("/update_time_limit")   #タスク期限変更(自由に変更可)
+async def update_time_limit(
+
+):
+    pass
+
+@router.put("/update_task_content") #タスク内容変更(自由に変更可)
+async def update_task_content(
+
+):
+    pass
 
 @router.delete("/tasks/{task_id}", response_model=None)
 async def delete_task(task_id: int, db: AsyncSession = Depends(get_db)):
