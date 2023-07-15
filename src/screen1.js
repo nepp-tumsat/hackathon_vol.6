e9aa2a657f7243ac0270e399094b6fdd31f29de5
Fujiken0705 <kentofujita.new@gmail.com>
2023-07-15 18:33:00 +0900

    バリデーション追加

--- src/screen1.js
+++ src/screen1.js
@@ -1,7 +1,20 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './screen1.css';
 
 const Screen1 = () => {
+    const [name, setName] = useState("");
+    const [job, setJob] = useState("");
+    const [hobby, setHobby] = useState("");
+    const [hobbyCost, setHobbyCost] = useState("");
+
+    const handleClick = () => {
+        if (!name || !job || !hobby || !hobbyCost) {
+            alert("全てのフィールドを入力してください");
+        } else {
+            window.location.href = '/screen2';
+        }
+    };
+
     return (
         <div className='container'>
             <div className='title'>
@@ -9,12 +22,12 @@ const Screen1 = () => {
                 <p>やることを<span className='highlight'>見える化</span>しよう！<br />下の表に書き、さあ始めよう！</p>
             </div>
             <div className='input-fields'>
-                <input type='text' placeholder='名前' />
-                <input type='text' placeholder='職業' />
-                <input type='text' placeholder='趣味' />
-                <input type='text' placeholder='趣味にかけるお金' />
+                <input type='text' placeholder='名前' value={name} onChange={e => setName(e.target.value)} />
+                <input type='text' placeholder='職業' value={job} onChange={e => setJob(e.target.value)} />
+                <input type='text' placeholder='趣味' value={hobby} onChange={e => setHobby(e.target.value)} />
+                <input type='text' placeholder='趣味にかけるお金' value={hobbyCost} onChange={e => setHobbyCost(e.target.value)} />
             </div>
-            <button className='continue-button' onClick={() => window.location.href='/screen2'}>
+            <button className='continue-button' onClick={handleClick}>
                 To be continue... <span>→</span>
             </button>
         </div>
